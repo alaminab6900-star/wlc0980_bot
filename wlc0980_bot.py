0cc362c4e22e433e38c6d3784ad49f83ffbb258a
alaminab6900-star <alaminab6900@gmail.com>
2026-06-02 15:18:46 +0600

    Update wlc0980_bot.py

--- wlc0980_bot.py
+++ wlc0980_bot.py
@@ -5,10 +5,10 @@ import requests
 import os
 from telegram import Bot
 
-TOKEN = os.environ.get("8834165485:AAG1gmI8F-mWOi2sbpLdxAodK43nPYHr5M4")
+TOKEN = os.environ.get("TOKEN")
 CHANNEL_ID = "@financebotai0"
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=8834165485:AAG1gmI8F-mWOi2sbpLdxAodK43nPYHr5M4)
 
 # 🔹 RSS FEEDS (change/add more)
 RSS_FEEDS = [
